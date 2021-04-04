77f5bc03e520dea80acf38280fd5ac773d464f6c
makmary <makmary05@gmail.com>
2021-04-05 01:33:19 +0300

    [add] script for server

---
+++ server_script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "		Settings for server"
+
+if ! [ $(id -u) = 0 ]; then 
+	echo "This script must be run as root"
+	exit 1
+else
+	#Update and Upgrade
+	echo "Updating and Upgrading..."
+	apt-get update && sudo apt-get upgrade -y	
+	
+	#Install Net-Tools
+	echo "Installing Net-Tools..."
+	apt install -y net-tools
+	
+	#Install Wireshark
+	echo "Installing Wireshark..."
+	apt install -y wireshark
+	
+	#Install Dnsmasq
+	echo "Installing DnsMasq..."
+	apt install -y dnsmasq
+fi	
